b6e204e6e1c29e2f1ca1b24295f663b9719809ba
Marlon Lima <goncalvesmarlon072@gmail.com>
2025-07-13 20:03:58 -0300

    Finalizado a primeira versão

---
+++ MoneyExtension/MoneyExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace MoneyExtension;
+
+public static class MoneyExtensions
+{
+    public static int ToCents(this decimal amount)
+    {
+        var text = amount
+            .ToString("N2")
+            .Replace(",", "")
+            .Replace(".", "");
+
+        if (string.IsNullOrEmpty(text))
+            return 0;
+
+        int.TryParse(text, out var result);
+        return result;
+    }
+}
